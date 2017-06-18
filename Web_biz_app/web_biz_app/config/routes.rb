be07f5ddde74c64427ec1c3ec92084e94f503bfa
Sentaro YOJIMA <yojimasentaro@gmail.com>
2017-06-18 18:00:56 +0900

    initialize views

--- Web_biz_app/web_biz_app/config/routes.rb
+++ Web_biz_app/web_biz_app/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "top#index"
+  root 'photos#index'
+
+  get  '/photos/index'  =>    'photos#index'
+  get  '/photos/new'    =>    'photos#new'
+  get  '/photos/show'   =>    'photos#show'
 end
