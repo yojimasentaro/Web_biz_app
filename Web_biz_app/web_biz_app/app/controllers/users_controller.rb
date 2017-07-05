cc8786a7dafbb3b6f215e83acc1bacfebb0c87fe
Sentaro YOJIMA <yojimasentaro@gmail.com>
2017-07-05 14:25:38 +0900

    define user controller

--- Web_biz_app/web_biz_app/app/controllers/users_controller.rb
+++ Web_biz_app/web_biz_app/app/controllers/users_controller.rb
@@ -1,2 +1,28 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!, only: [:edit, :update]
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    current_user.update(update_params)
+    redirect_to root_path, notice: 'Updated Successfully!'
+  end
+
+  private
+  def update_params
+    params.require(:user).permit(
+      :username,
+      :avatar,
+      :email,
+      :password,
+      :profile,
+      :member,
+      :works
+      )
+  end
 end
