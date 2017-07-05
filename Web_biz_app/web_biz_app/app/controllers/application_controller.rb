403e64a502c7ab7f9712da82e859f208f5db1b62
Sentaro YOJIMA <yojimasentaro@gmail.com>
2017-07-05 14:25:20 +0900

    define app controller

--- Web_biz_app/web_biz_app/app/controllers/application_controller.rb
+++ Web_biz_app/web_biz_app/app/controllers/application_controller.rb
@@ -1,3 +1,23 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  add_flash_types :success, :info, :warning, :danger
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.for(:sign_up).push(
+        :username,
+        :member,
+        :profile,
+        :works,
+        :avatar
+        )
+      devise_parameter_sanitizer.for(:account_update).push(
+        :username,
+        :avatar,
+        :member,
+        :profile,
+        :works
+        )
+    end
 end
