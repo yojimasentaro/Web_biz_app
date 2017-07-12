c639a9acc11f49e4cb35d7cdac81fe47ebdb5af6
ima-s <shigeaki.imakiire1109@gmail.com>
2017-07-12 22:41:53 +0900

    added search method for Photo

--- Web_biz_app/web_biz_app/app/models/photo.rb
+++ Web_biz_app/web_biz_app/app/models/photo.rb
@@ -1,3 +1,4 @@
+# coding: utf-8
 class Photo < ApplicationRecord
   acts_as_taggable
   belongs_to :user
@@ -21,4 +22,36 @@ class Photo < ApplicationRecord
    likes.find_by(user_id: user)
   end
 
+  def self.search(keyword)
+    photo_ids = [] # 検索でヒットしたユーザーのidを格納する配列
+    
+    #入力がある場合とない場合で分ける
+    if keyword.presence
+    #入力文字列を空白で区切ってリストに格納
+      patterns = keyword.split(/[ , ]/)
+      sql_body = ''
+      tagged_photos = []
+
+      #SQL文を作成する
+      patterns.each do |pattern|
+        sql_body += ' and ' unless sql_body.blank?
+        sql_body += " title like '%#{pattern}%' "
+        tagged_photos.push(Photo.tagged_with(pattern)) # "pattern"というタグを持つPhotoのリストがpushされる
+      end
+      sql = "select * from photos where #{sql_body} order by id desc"
+    
+    #find_by_sqlで条件を満たすユーザーを取得し、それらのidを取得する
+      photo_ids << Photo.find_by_sql(sql).map(&:id)
+
+      # リストをidに変換
+      tagged_photos.each do |photo|
+        photo_ids << photo.ids
+      end
+
+      photo_ids.flatten!
+      where(:id => photo_ids)
+    else
+      Photo.all
+    end
+  end    
 end
